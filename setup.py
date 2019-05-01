8e689db1c7868942835cb544331ff0bdec9022c2
Cong Chen <congchen@google.com>
2019-04-30 22:59:33 -0400

    Bump up version for PyPI release.

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ ENTRY_POINTS = {
 
 setup(
     name='election_results_xml_validator',
-    version='0.2.9.4',
+    version='0.2.9.5',
     author='Google Civics',
     author_email='election-results-xml-validator@google.com',
     maintainer='gVelocity Civics',
