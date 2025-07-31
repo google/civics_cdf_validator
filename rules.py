354bb40e347885086c245b918c78cc18604e6977
Bobby Panczer <panczer@google.com>
2025-07-31 10:05:58 -0700

    Change `OfficeRole` to `Role` in post-split feed validation logic to align with external documentation.
    PiperOrigin-RevId: 789379473

--- rules.py
+++ rules.py
@@ -98,7 +98,7 @@ _VALID_OFFICE_ROLE_COMBINATIONS = frozenset([
 
 def _get_office_roles(element, is_post_office_split_feed=False):
   if is_post_office_split_feed:
-    return [element.text for element in element.findall("OfficeRole")]
+    return [element.text for element in element.findall("Role")]
   return get_entity_info_for_value_type(element, "office-role")
 
 
