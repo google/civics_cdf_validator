9d10c6a5d8da1d1d0c268b1040507579fe185a44
Civics Team <civics-copybara@google.com>
2026-04-20 13:14:54 -0700

    Project import generated by Copybara.
    Release-Id: civics_cdf_validator_20260420_prod_RC00

--- rules.py
+++ rules.py
@@ -97,7 +97,7 @@ _EXECUTIVE_OFFICE_ROLES = frozenset([
 _VALID_FEED_LONGEVITY_BY_FEED_TYPE = frozendict({
     "committee": ["evergreen"],
     "election-dates": ["evergreen"],
-    "election-results": ["limited", "yearly"],
+    "election-results": ["limited"],
     "officeholder": ["evergreen"],
     "pre-election": ["limited", "yearly"],
 })
@@ -2155,19 +2155,19 @@ class VoteCountValidSeatsDeltaTypes(base.BaseRule):
           raise loggers.ElectionError.from_message(msg, [element])
         if "seats-delta" in ballot_selection_vote_types:
           current_date = datetime.datetime.now()
-          deletion_date = datetime.datetime(2026, 6, 1)
-          #   If current date is before June 1st, 2026
+          deletion_date = datetime.datetime(2026, 7, 1)
+          #   If current date is before July 1st, 2026
           if current_date < deletion_date:
             msg = (
                 "VoteCount type seats-delta is deprecated and will be removed"
-                " on June 1, 2026. Please update your implementation to use"
+                " on July 1, 2026. Please update your implementation to use"
                 " seats-delta-mandate. (BallotSelection objectId={})"
             ).format(ballot_selection_obj_id)
             raise loggers.ElectionWarning.from_message(msg, [element])
           else:
             msg = (
                 "VoteCount type seats-delta is deprecated and was removed on"
-                " June 1, 2026. Please update your implementation to use"
+                " July 1, 2026. Please update your implementation to use"
                 " seats-delta-mandate. (BallotSelection objectId={})"
             ).format(ballot_selection_obj_id)
             raise loggers.ElectionError.from_message(msg, [element])
